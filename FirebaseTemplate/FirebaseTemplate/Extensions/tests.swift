815488bc7534ca3ec6a3bc2227b219ae5b5577c2
hAlmousawi12 <almousawi2006@gmail.com>
2021-03-10 18:49:29 +0300

    doneeeeeee

--- FirebaseTemplate/FirebaseTemplate/Extensions/tests.swift
+++ FirebaseTemplate/FirebaseTemplate/Extensions/tests.swift
@@ -8,19 +8,16 @@
 import SwiftUI
 
 struct tests: View {
-    var txt: String? = nil
     var body: some View {
-//        GeometryReader { geometry in
-//            ScrollView {
-//                Text("Hello, World!")
-//            }.frame(width: geometry.size.width - 20)
-//            .background(Color("offW&B"))
-//            .cornerRadius(15)
-//            .padding(.leading, 10)
-//            .shadow(color: Color("Shadow").opacity(0.5), radius: 10, x: 0, y: 10)
-//        }
-           Text(txt!)
-        
+        GeometryReader { geometry in
+            ScrollView {
+                Text("Hello, World!")
+            }.frame(width: geometry.size.width - 20)
+            .background(Color("offW&B"))
+            .cornerRadius(15)
+            .padding(.leading, 10)
+            .shadow(color: Color("Shadow").opacity(0.5), radius: 10, x: 0, y: 10)
+        }
     }
 }
 
