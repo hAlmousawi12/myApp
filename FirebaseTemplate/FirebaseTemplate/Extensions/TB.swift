21ad0f118117b822fc873c93d3e09278b1edeb4f
hAlmousawi12 <almousawi2006@gmail.com>
2021-03-14 10:21:53 +0300

    frgthj

--- FirebaseTemplate/FirebaseTemplate/Extensions/TB.swift
+++ FirebaseTemplate/FirebaseTemplate/Extensions/TB.swift
@@ -6,6 +6,67 @@
 //
 
 
+//import SwiftUI
+//
+//var tabs = ["calendar", "textformat.123", "star", "person.circle"]
+//var tabsName = ["Calendar", "Standings", "Favorite club", "Profile"]
+//struct TB: View {
+//    @State var selectedTab = "calender"
+//    @State var edge = UIApplication.shared.windows.first?.safeAreaInsets
+//    var body: some View {
+//
+//        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+//            TabView(selection: $selectedTab) {
+//                Calender()
+//                    .tag("calendar")
+//                Standings()
+//                    .tag("textformat.123")
+//                FC()
+//                    .tag("star")
+//                Profile()
+//                    .tag("person.circle")
+//            }
+//            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+//            .ignoresSafeArea(.all, edges: .all)
+//            HStack {
+//                Spacer()
+//                ForEach(0..<tabs.count) { i in
+//                    TabButton(image: tabs[i], text: tabsName[i], selectedTab: $selectedTab)
+//                    Spacer()
+//                }
+//            }
+//            .padding(.horizontal, 25)
+//            .padding(.vertical, 5)
+//            .background(BlurView(style: .systemChromeMaterial))
+//            .cornerRadius(25)
+//            .shadow(color: Color.black.opacity(0.15), radius: 5, x: 5, y: 5)
+//            .shadow(color: Color.black.opacity(0.15), radius: 5, x: -5, y: -5)
+//            .padding(.horizontal)
+//        }
+//    }
+//}
+//
+//struct TabButton: View {
+//    var image: String
+//    var text: String
+//    @Binding var selectedTab: String
+//    var body: some View {
+//        VStack {
+//            Button(action: {
+//                    selectedTab = image
+//                }, label: {
+//                    Image(systemName: image)
+//
+//            })
+//            Text(text)
+//                .font(.footnote)
+//        }
+//        .padding(.vertical)
+//        .foregroundColor(selectedTab == image ? Color("Primary") : Color("Primary3ks"))
+//    }
+//}
+
+
 import SwiftUI
 
 struct TB: View {
